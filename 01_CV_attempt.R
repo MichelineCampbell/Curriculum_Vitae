22867443b11b5c2bdae50e924f88008276191f45
MichelineCampbell <michelineleecampbell@gmail.com>
2020-04-09 15:23:37 +1000

    Rstudio commit

--- 01_CV_attempt.R
+++
@@ -1,9 +0,0 @@
-## Script Name: 01_CV_attempt.R
-## Purpose: Create CV in using markdown
-## Initial Author: Dr. Micheline Campbell
-## Contributing Authors: 
-## Date Created: 20200229
-## Last Edited:
-##  By: MC
-## Email: michelineleecampbell@gmail.com
-## Notes: 
